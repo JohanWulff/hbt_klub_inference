2f12504a1f55e205bd6dcc89fe3ec0c083b4cec0
Johan Wulff <jwulff@lip.pt>
2023-11-08 00:20:40 +0100

    update branchnames

--- add_branch/bin/addBranch.cc
+++ add_branch/bin/addBranch.cc
@@ -9,7 +9,7 @@ void show_help() {
     /* Show help for input arguments */
     std::cout << "-i : input root file (containing predictions), no default \n";
     std::cout << "-t : target root file, no default \n";
-    std::cout << "-n : branch name, default: nonparam_baseline \n";
+    std::cout << "-n : model name, no default\n";
     std::cout << "-p : parametrised, default: false \n";
 }
 
@@ -18,7 +18,7 @@ std::map<std::string, std::string> get_options(int argc, char* argv[]) {
     /*Interpret input arguments*/
 
     std::map<std::string, std::string> options;
-    options.insert(std::make_pair("-i", std::string())); // model root name
+    options.insert(std::make_pair("-i", std::string()));
     options.insert(std::make_pair("-t", std::string()));
     options.insert(std::make_pair("-n", std::string("nonparam_baseline")));
     options.insert(std::make_pair("-p", std::string("false")));
@@ -46,68 +46,62 @@ std::map<std::string, std::string> get_options(int argc, char* argv[]) {
 }
 
 
-bool fill_new_branch(std::string targ_file, std::string predictions_file) {
-    //if (!boost::filesystem::exists(targ_file)) {
-        //throw std::invalid_argument("File: " + targ_file + " not found");
-        //return false;
-    //}
+//bool fill_new_branch(std::string targ_file, std::string predictions_file) {
+//    //if (!boost::filesystem::exists(targ_file)) {
+//        //throw std::invalid_argument("File: " + targ_file + " not found");
+//        //return false;
+//    //}
+//
+//    Int_t Run,  Lumi, fRun, fLumi;
+//    ULong64_t Event, fEvent;
+//    Double_t dnn_output; 
+//
+//    std::cout << "Opening " << targ_file << "\n";
+//    TFile *f = new TFile(targ_file.c_str(), "update");
+//    TTree *klub_tree  = (TTree*)f->Get("HTauTauTree");
+//    TBranch *newbranch = klub_tree->Branch("DNN", &dnn_output, "DNN/F");
+//
+//    std::cout << "Opening " << predictions_file << "\n";
+//    TFile *pred_file = new TFile(predictions_file.c_str());
+//    TTree *pred_tree = (TTree*)pred_file->Get("evaluation");
+//
+//    klub_tree->SetBranchAddress("RunNumber",&Run);
+//    klub_tree->SetBranchAddress("EventNumber",&Event);
+//    klub_tree->SetBranchAddress("lumi",&Lumi);
+//    pred_tree->SetBranchAddress("RunNumber",&fRun);
+//    pred_tree->SetBranchAddress("EventNumber",&fEvent);
+//    pred_tree->SetBranchAddress("lumi",&fLumi);
+//    pred_tree->SetBranchAddress("dnn_output", &dnn_output);
+//    klub_tree->AddFriend(pred_tree);
+//    
+//    Long64_t nentries = klub_tree->GetEntries();
+//    for (Long64_t i=0;i<nentries;i++) {
+//        klub_tree->GetEntry(i);
+//        if (fRun == Run && fEvent==Event && fLumi==Lumi) {
+//            pred_tree->GetEntryWithIndex(Run,Event);
+//            newbranch->Fill();
+//        }
+//        else{
+//            printf("Files don't seem to match");
+//        }
+//    }
+//    delete f;
+//    delete pred_file; 
+//    return true;
+//}
 
-    Int_t Run,  Lumi, fRun, fLumi;
-    ULong64_t Event, fEvent;
-    Double_t dnn_output; 
 
-    std::cout << "Opening " << targ_file << "\n";
-    TFile *f = new TFile(targ_file.c_str(), "update");
-    TTree *klub_tree  = (TTree*)f->Get("HTauTauTree");
-    TBranch *newbranch = klub_tree->Branch("DNN", &dnn_output, "DNN/F");
-
-    std::cout << "Opening " << predictions_file << "\n";
-    TFile *pred_file = new TFile(predictions_file.c_str());
-    TTree *pred_tree = (TTree*)pred_file->Get("evaluation");
-
-    klub_tree->SetBranchAddress("RunNumber",&Run);
-    klub_tree->SetBranchAddress("EventNumber",&Event);
-    klub_tree->SetBranchAddress("lumi",&Lumi);
-    pred_tree->SetBranchAddress("RunNumber",&fRun);
-    pred_tree->SetBranchAddress("EventNumber",&fEvent);
-    pred_tree->SetBranchAddress("lumi",&fLumi);
-    pred_tree->SetBranchAddress("dnn_output", &dnn_output);
-    klub_tree->AddFriend(pred_tree);
-    
-    Long64_t nentries = klub_tree->GetEntries();
-    for (Long64_t i=0;i<nentries;i++) {
-        klub_tree->GetEntry(i);
-        if (fRun == Run && fEvent==Event && fLumi==Lumi) {
-            pred_tree->GetEntryWithIndex(Run,Event);
-            newbranch->Fill();
-        }
-        else{
-            printf("Files don't seem to match");
-        }
-    }
-    delete f;
-    delete pred_file; 
-    return true;
-}
-
-
-bool fill_new(std::string targ_file, std::string predictions_file, std::string branch_name) {
-    //if (!boost::filesystem::exists(targ_file)) {
-        //throw std::invalid_argument("File: " + targ_file + " not found");
-        //return false;
-    //}
+bool fill_new(std::string targ_file, std::string predictions_file, std::string model_name) {
 
     Double_t dnn_output; 
 
-    std::cout << "Opening " << predictions_file << "\n";
     TFile *pred_file = new TFile(predictions_file.c_str());
-    TTree *pred_tree = (TTree*)pred_file->Get("evaluation");
-    pred_tree->SetBranchAddress(std::string("dnn_"+branch_name).c_str(), &dnn_output);
+    TTree *pred_tree = (TTree*)pred_file->Get("KLUB_evaluation");
+    pred_tree->SetBranchAddress(std::string("dnn_"+model_name).c_str(), &dnn_output);
 
-    std::cout << "Opening " << targ_file << "\n";
     TFile *f = new TFile(targ_file.c_str(), "update");
     TTree *klub_tree  = (TTree*)f->Get("HTauTauTree");
-    TBranch *newbranch = klub_tree->Branch(std::string("dnn_"+branch_name).c_str(), &dnn_output, std::string("dnn_"+branch_name+"/D").c_str());
+    TBranch *newbranch = klub_tree->Branch(std::string("dnn_"+model_name).c_str(), &dnn_output, std::string("dnn_"+model_name+"/D").c_str());
 
     
     Long64_t nentries = klub_tree->GetEntries();
@@ -132,8 +126,13 @@ int main(int argc, char *argv[]){
     if (options.size() == 0) {
         return 1;
     }
-    std::cout << "Running add_branch \n";
+    std::cout << "Running add_branch with options:\n";
     bool parametrised = options["-p"] == "true";
+    std::cout << "input file (-i): " << options["-i"] << "\n";
+    std::cout << "target file (-t): " << options["-t"] << "\n";
+    std::cout << "name (-n): " << options["-n"] << "\n";
+    std::cout << "parametrised (-p): " << options["-p"] << "\n";
+
     if (parametrised){
         int masses[25] = {250, 260, 270, 280, 300, 320, 350, 400,
                           450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 1000,
